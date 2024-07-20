e86f17d3759247a51b1c369ffa4d65e266082cce
Jakub Zedník <91053389+kuboi1@users.noreply.github.com>
2024-07-20 22:08:02 +0200

    update notification logic

--- ticket_watcher.py
+++ ticket_watcher.py
@@ -80,14 +80,13 @@ def send_on_sale_email(config: dict, event: dict):
 def main(config: dict):
     events = config['events']
     check_frequency = config['checkFrequency'] # Check frequency in seconds
-    email_frequency = config['emailFrequency'] # Email notification frequency in seconds
     headers = BASE_HEADERS
 
     # Add the bearer token auth header
     headers['Authorization'] = f'Bearer {config["bearerToken"]}'
 
     # Init last notification dict
-    last_notifications = {event['id']: 0 for event in events}
+    last_notifications = {event['id']: True for event in events}
 
     while True:
         # Check availability for all events
@@ -102,18 +101,22 @@ def main(config: dict):
             )
 
             if response.status_code == 200:
-                event = response.json()
-                sale_status = event['saleStatus']
+                response_event = response.json()
+                sale_status = response_event['saleStatus']
 
                 log(f'Event is {SALE_STATUSES[sale_status]}!')
 
-                # Send email notification if event available and last notification was sent before email frequency seconds
-                if sale_status == SALE_STATUS_ON_SALE and time.time() - last_notifications[event['id']] > email_frequency:
+                # Send email notification if event available and notification wasn't yet sent
+                if sale_status == SALE_STATUS_ON_SALE and last_notifications[event['id']]:
                     # Send notification email
                     send_on_sale_email(config['email'], event)
 
                     # Save current time as last notification time for event
-                    last_notifications[event['id']] = time.time()
+                    last_notifications[event['id']] = False
+                
+                # Reset notification sent
+                if sale_status == SALE_STATUS_SOLD_OUT and not last_notifications[event['id']]:
+                    last_notifications[event['id']] = True
             else:
                 log(f'ERROR: There was an error with a request to {url}. Request failed with status {response.status_code}: {response.text}')
         
